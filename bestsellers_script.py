e0e9fc97439c0079e7936b325e8c7d24e23d3739
ingaprivet <inga.poznyak@gmail.com>
2020-06-21 18:21:00 -0400

    Updated

--- bestsellers_script.py
+++ bestsellers_script.py
@@ -2,7 +2,7 @@ from time import sleep
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-# init driver
+# initialize the driver
 driver = webdriver.Chrome(
     executable_path='/Users/ingabukhvalova/PycharmProjects/python-selenium-automation/chromedriver')
 driver.maximize_window()
