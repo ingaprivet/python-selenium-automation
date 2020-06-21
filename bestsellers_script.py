8498b944e3c89f72a76123c56818357135456e4e
ingaprivet <inga.poznyak@gmail.com>
2020-06-21 17:39:30 -0400

    “Homework

---
+++ bestsellers_script.py
@@ -0,0 +1,18 @@
+from time import sleep
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+# init driver
+driver = webdriver.Chrome(
+    executable_path='/Users/ingabukhvalova/PycharmProjects/python-selenium-automation/chromedriver')
+driver.maximize_window()
+
+# open the url
+driver.get('https://www.amazon.com/gp/bestsellers/?ref_=nav_cs_bestsellers')
+driver.implicitly_wait(4)
+
+bestsellers_text = (By.CSS_SELECTOR, "div#zg_banner_text")
+bestsellers_links = (By.CSS_SELECTOR, "a[href*='zg_bs_tab']")
+
+
+driver.quit()
---
+++ features/tests/bestsellers.feature
@@ -0,0 +1,7 @@
+# Created by ingabukhvalova at 6/19/20
+Feature: Test Scenarios for BestSellers page
+
+  Scenario: User can open BestSeller page and see
+    Given Open amazon BestSellers page
+    When Verify Zeitgeist menu is displayed
+    Then Verify 5 links
