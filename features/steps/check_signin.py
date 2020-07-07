d4857f8e9c88be1ff8b10ce92326bb67afc649b4
ingaprivet <inga.poznyak@gmail.com>
2020-07-06 23:51:32 -0400

    Homework 7

--- features/steps/check_signin.py
+++ features/steps/check_signin.py
@@ -1,16 +1,11 @@
 from selenium.webdriver.common.by import By
 from behave import given, when, then
-from time import sleep
 
-SEARCH_INPUT = (By.ID, 'nav-orders')
-SEARCH_SUBMIT = (By.XPATH, "//a[@id='nav-link-accountList']")
-
-@given('Open Amazon page to check sign in')
-def open_amazon(context):
-    context.driver.get('https://www.amazon.com')
-
-@when('Click Orders open the Sign-in page')
+@when('Click Orders')
 def click_search_icon(context):
-    context.driver.find_element(*SEARCH_SUBMIT).click()
-    sleep(1)
+    context.app.sign_in_services.click_orders_link()
 
+
+@then('Verify {search_text} page is opened')
+def verify_found_results_text(context, search_text):
+    context.app.sign_in_services.verify_found_results_text(search_text)
