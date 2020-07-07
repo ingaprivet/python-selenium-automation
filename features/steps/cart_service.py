202a49884f1ddc0ee378a1628f28ebb8bb74c149
ingaprivet <inga.poznyak@gmail.com>
2020-07-06 23:50:44 -0400

    Homework 7

--- features/steps/cart_service.py
+++ features/steps/cart_service.py
@@ -1,24 +1,19 @@
-from time import sleep
-
 from behave import given, when, then
 from selenium.webdriver.common.by import By
 
-SHOPPING_CART_ICON = (By.ID, 'nav-cart')
-EMPTY_CART_TEXT = (By.CSS_SELECTOR, "div.a-row.sc-your-amazon-cart-is-empty")
 
-
-@given('Open Amazon page')
+@given('Open Amaxon page')
 def open_amazon(context):
-    context.driver.get('https://www.amazon.com')
+    context.app.page.open_page()
 
 
 @when('Click on Cart Icon')
-def click_search_icon(context):
-    context.driver.find_element(*SHOPPING_CART_ICON).click()
-    sleep(4)
+def click_cart_icon(context):
+    context.app.cart_services.click_cart_icon()
 
 
-@then('Verify the Shopping Cart is empty')
-def verify_found_results_text(context):
-    search_result_header = context.driver.find_element(*EMPTY_CART_TEXT).text
-    assert 'Your Amazon Cart is empty' in search_result_header, f'Incorrect page is found: {search_result_header}'
+@then('Verify {search_text} text present')
+def verify_found_results_text(context, search_text):
+    context.app.cart_services.verify_found_results_text(search_text)
+
+
