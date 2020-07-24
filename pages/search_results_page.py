6fea0067c1e46dee765aa971700ca7a7e4cadabf
ingaprivet <inga.poznyak@gmail.com>
2020-07-24 15:51:46 -0400

    Homework 9

--- pages/search_results_page.py
+++ pages/search_results_page.py
@@ -6,7 +6,6 @@ class SearchResults(Page):
     RESULTS_FOUND_MESSAGE = (By.CSS_SELECTOR, ".a-color-state.a-text-bold")
 
     def verify_found_results_text(self, search_world):
-        print("in verify of SearchResults")
         search_result_header = self.find_element(*self.RESULTS_FOUND_MESSAGE).text
         assert search_world in search_result_header, f'Incorrect header: {search_result_header}'
 
